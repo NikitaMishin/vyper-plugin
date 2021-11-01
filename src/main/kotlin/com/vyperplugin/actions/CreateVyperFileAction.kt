d5a72b80d79ca84c673f768f79d6d67af6f8ecef
rudolf101 <st087506@student.spbu.ru>
2021-11-01 23:56:25 +0300

    Update CreateVyperFileAction.kt

--- src/main/kotlin/com/vyperplugin/actions/CreateVyperFileAction.kt
+++ src/main/kotlin/com/vyperplugin/actions/CreateVyperFileAction.kt
@@ -11,11 +11,6 @@ private const val name = "Create empty Vyper file"
 private const val description = "Empty Vyper file"
 
 class CreateVyperFileAction : CreateFileFromTemplateAction(name, description, VyperIcons.FILE) {
-//    override fun getActionName(directory: PsiDirectory?, newName: String?, templateName: String?): String = name
-//    override fun buildDialog(project: Project?, directory: PsiDirectory?, builder: CreateFileFromTemplateDialog.Builder) {
-//        builder.setTitle(name).addKind("Empty file", VyperIcons.FILE, "Vyper File")
-//
-//    }
 
     override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
         builder.setTitle(name).addKind("Empty file", VyperIcons.FILE, "Vyper File")
